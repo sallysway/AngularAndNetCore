5abc9c9a27382b530827c5f13efc4493e8693028
sallysway <ioanaandreea476@gmail.com>
2021-05-14 12:14:28 +0300

    Added support for troubleshooting server error

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -20,8 +20,7 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe(response => {
       this.router.navigateByUrl('/members');
     }, error => {
-      console.log(error);
-      this.toastr.error(error.error)
+      //do something with error
     });
   }
 
